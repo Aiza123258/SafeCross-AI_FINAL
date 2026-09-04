492edad68a005f00aa25eb22df141ddaaeac182a
Aiza123258 <aizaazmat3@gmail.com>
2026-09-04 04:41:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ AI-Powered Intelligent Road Safety & Emergency Management Platform.
 
 import streamlit as st
 import sys
-import os
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from utils.ui_components import (
